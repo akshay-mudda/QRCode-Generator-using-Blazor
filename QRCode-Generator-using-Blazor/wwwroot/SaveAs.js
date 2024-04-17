aee1911ed0cf8fdf889f0238128cae5f1224b8ff
akshaymu <akshaymu@ICS-LT-66Q1ZD3>
2024-04-17 17:55:29 +0530

    Add project files.

---
+++ QRCode-Generator-using-Blazor/wwwroot/SaveAs.js
@@ -0,0 +1,16 @@
+﻿function saveAs(content, filename) {
+    // Create a Blob from the byte array
+    const blob = new Blob([content], { type: 'application/octet-stream' });
+
+    // Create an object URL for the Blob
+    const url = window.URL.createObjectURL(blob);
+
+    // Create a link element
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = filename || 'download';
+    a.click();
+
+    // Revoke the object URL
+    window.URL.revokeObjectURL(url);
+}
